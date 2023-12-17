5ef3679bb846d8464a6b8b2720c64d8f0da5e695
zain100000 <muhammadzainulabideen292@gmail.com>
2023-12-17 15:56:43 +0500

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,28 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-
-// const placesRoutes = require('./routes/places-routes');
-// const usersRoutes = require('./routes/users-routes');
-// const HttpError = require('./models/http-error');
-
-// const server = express();
-
-// server.use(bodyParser.json());
-
-// server.use('/api/places', placesRoutes); // => /api/places...
-// server.use('/api/users', usersRoutes); // => /api/places...
-
-// server.use((req, res, next) => {
-//   const error = new HttpError('Could not find this route.', 404);
-//   throw error;
-// });
-
-// server.use((error, req, res, next) => {
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500)
-//   res.json({ message: error.message || 'An unknown error occurred!' });
-// });
-
-// server.listen(5000);
-
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoPractice = require("./mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const server = express();
 
 server.use(bodyParser.json());
 
-server.post("/products", mongoPractice.createProducts);
+server.use("/api/places", placesRoutes); // => /api/places...
+server.use("/api/users", usersRoutes); // => /api/places...
 
-server.get("/products", mongoPractice.getProducts);
+server.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
 
-server.listen(3000);
+server.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+server.listen(5000);
